172405aacef673e0cbdcd692565f7270cea85166
Bernd Malle <bernd.malle@gmail.com>
2019-08-27 15:01:18 +0200

    added jaccard on Uint32Arrays - on (symmetrically split) size of 1e5 it is about ~2x faster

--- src/recommender/Similarities.ts
+++ src/recommender/Similarities.ts
@@ -1,6 +1,43 @@
-export function jaccard(a: Set<any>, b: Set<any>) {
+
+export function jaccard(a: Set<any> | Map<any, any>, b: Set<any> | Map<any, any>) {
 	const unionSize = new Set([...a, ...b]).size;
 	const intersectSize = a.size + b.size - unionSize;
 	return intersectSize / unionSize;
 }
 
+
+
+
+/**
+ * @works, but we would have to completely re-vamp Graphinius typed edge traversals
+ * 				 in order to speed the code up by a factor of ~2...
+ * @todo Fuck speed for the moment -> concern yourself with optimization ->
+ * 					!!! AFTER THE FUCKING DEMO !!!
+ * @todo I think this doesn't pay off in any way...
+ */
+export function jaccardI32(a: Uint32Array, b: Uint32Array) {
+	a = a.sort();
+	b = b.sort();
+	const union = [];
+	let
+		i = 0,
+		j = 0;
+	while ( i < a.length || j < b.length ) {
+		if ( i >= a.length ) {
+			union.push(b[j++]);
+		}
+		else if ( j >= b.length ) {
+			union.push(a[i++]);
+		}
+		else {
+			union.push(a[i]);
+			if (a[i++] !== b[j]) {
+				union.push(b[j++]);
+			}
+			else {
+				j++;
+			}
+		}
+	}
+	return (a.length + b.length - union.length) / union.length;
+}
--- test/similarities/Similarities.test.ts
+++ test/similarities/Similarities.test.ts
@@ -1,15 +1,45 @@
-import {jaccard} from "../../src/recommender/Similarities";
+import {jaccard, jaccardI32} from "../../src/recommender/Similarities";
 
 describe('SET similarity tests', () => {
 
+	const
+		a = [1,2,3],
+		b = [1,2,4,5],
+		c = [],
+		d = [],
+		SUPER_SIZE = 1e5;
+	let	i = SUPER_SIZE;
+	while ( i-- >= SUPER_SIZE/4 ) d.push(i);
+	while ( i-- ) c.push(i);
+	let
+		s_a = new Set(a),
+		s_b = new Set(b),
+		s_c = new Set(c),
+		s_d = new Set(d),
+		t_ai = new Uint32Array(a),
+		t_bi = new Uint32Array(b),
+		t_ci = new Uint32Array(c),
+		t_di = new Uint32Array(d);
+
+
 	describe('Jaccard similarity tests', () => {
 
-		it('should compute a simple jaccard similarity', () => {
-			const
-				a = new Set([1, 2, 3]),
-				b = new Set([1, 2, 4, 5]);
-			expect(jaccard(a, b)).toBe(0.4);
+		it('should compute jaccard between two simple SETS', () => {
+			expect(jaccard(s_a, s_b)).toBe(0.4);
 		});
+
+		it('should compute jaccard between two LARGE sets', () => {
+			expect(jaccard(s_c, s_d)).toBe(0);
+		});
+
+		it('should compute jaccard between two simple TypedArraysI32', () => {
+			expect(jaccardI32(t_ai, t_bi)).toBe(0.4);
+		});
+
+		it('should compute jaccard between two LARGE TypedArraysI32', () => {
+			expect(jaccardI32(t_ci, t_di)).toBe(0);
+		});
+
 
 	});
 
